4bb6514c0ae99255f53138d61c8ef2053c371f7a
Hoger Dehnhardt <holger@dehnhardt.org>
2018-02-09 18:23:49 +0100

    add functional name and position to model allow serialiszing to xml

--- src/model/FLCModuleInstance.cpp
+++ src/model/FLCModuleInstance.cpp
@@ -1,47 +1,58 @@
 #include "FLCModuleInstance.h"
 
-FLCModuleInstance::FLCModuleInstance(QObject *parent) : QObject(parent)
+FLCModuleInstance::FLCModuleInstance(QObject *parent)
+	: QObject(parent),
+	  m_uuid(QUuid())
 {
+
 }
+
+void FLCModuleInstance::serialize( QXmlStreamWriter &xmlWriter )
+{
+	xmlWriter.writeStartElement(moduleFunctionalName());
+	xmlWriter.writeAttribute("uuid", uuid().toString());
+	xmlWriter.writeAttribute("name", moduleName());
+	xmlWriter.writeStartElement("moduleType");
+	xmlWriter.writeAttribute("moduleTypeId", QString(static_cast<int>(moduleType())));
+	xmlWriter.writeCharacters(moduleTypeName());
+	xmlWriter.writeEndElement();
+	xmlWriter.writeStartElement("moduleDataType");
+	xmlWriter.writeAttribute("moduleDataTypeId", QString(static_cast<int>(dataType())));
+	xmlWriter.writeEndElement();
+	xmlWriter.writeStartElement("displayPosition");
+	xmlWriter.writeAttribute("x", QString(position().x()));
+	xmlWriter.writeAttribute("y", QString(position().y()));
+	xmlWriter.writeEndElement();
+	xmlWriter.writeEndElement();
+}
+
+/*
+ *  getter
+ */
 
 QUuid FLCModuleInstance::uuid() const
 {
 	return m_uuid;
 }
 
-void FLCModuleInstance::setUuid(const QUuid &uuid)
+QString FLCModuleInstance::moduleFunctionalName() const
 {
-	m_uuid = uuid;
+	return m_sModuleFunctionalName;
 }
 
-std::string FLCModuleInstance::sModuleTypeName() const
+QString FLCModuleInstance::moduleTypeName() const
 {
 	return m_sModuleTypeName;
 }
 
-void FLCModuleInstance::setSModuleTypeName(const std::string &sModuleTypeName)
-{
-	m_sModuleTypeName = sModuleTypeName;
-}
-
-std::string FLCModuleInstance::sModuleName() const
+QString FLCModuleInstance::moduleName() const
 {
 	return m_sModuleName;
 }
 
-void FLCModuleInstance::setSModuleName(const std::string &sModuleName)
-{
-	m_sModuleName = sModuleName;
-}
-
-std::string FLCModuleInstance::sDescription() const
+QString FLCModuleInstance::description() const
 {
 	return m_sDescription;
-}
-
-void FLCModuleInstance::setSDescription(const std::string &sDescription)
-{
-	m_sDescription = sDescription;
 }
 
 flaarlib::MODULE_TYPE FLCModuleInstance::moduleType() const
@@ -49,9 +60,9 @@ flaarlib::MODULE_TYPE FLCModuleInstance::moduleType() const
 	return m_moduleType;
 }
 
-void FLCModuleInstance::setModuleType(const flaarlib::MODULE_TYPE &moduleType)
+QPoint FLCModuleInstance::position()
 {
-	m_moduleType = moduleType;
+	return m_position;
 }
 
 flaarlib::DATA_TYPE FLCModuleInstance::dataType() const
@@ -59,7 +70,52 @@ flaarlib::DATA_TYPE FLCModuleInstance::dataType() const
 	return m_dataType;
 }
 
+/*
+ * setter
+ */
+
+void FLCModuleInstance::setModuleTypeName(const QString &sModuleTypeName)
+{
+	m_sModuleTypeName = sModuleTypeName;
+}
+
+void FLCModuleInstance::setModuleName(const QString &sModuleName)
+{
+	m_sModuleName = sModuleName;
+}
+
+void FLCModuleInstance::setDescription(const QString &sDescription)
+{
+	m_sDescription = sDescription;
+}
+
+void FLCModuleInstance::setModuleType(const flaarlib::MODULE_TYPE &moduleType)
+{
+	m_moduleType = moduleType;
+}
+
+void FLCModuleInstance::setUuid(const QUuid &uuid)
+{
+	m_uuid = uuid;
+}
+
 void FLCModuleInstance::setDataType(const flaarlib::DATA_TYPE &dataType)
 {
 	m_dataType = dataType;
+}
+
+void FLCModuleInstance::setPosition(const QPoint &position)
+{
+	this->m_position = position;
+}
+
+void FLCModuleInstance::setPosition(const int x, const int y)
+{
+	this->m_position.setX(x);
+	this->m_position.setY(y);
+}
+
+void FLCModuleInstance::setModuleFunctionalName(const QString &sModuleFunctionalName)
+{
+	m_sModuleFunctionalName = sModuleFunctionalName;
 }
--- src/model/FLCModuleInstance.h
+++ src/model/FLCModuleInstance.h
@@ -5,7 +5,8 @@
 
 #include <QObject>
 #include <QUuid>
-
+#include <QPoint>
+#include <QXmlStreamWriter>
 
 class FLCModuleInstance : public QObject
 {
@@ -13,35 +14,45 @@ class FLCModuleInstance : public QObject
 public:
 	explicit FLCModuleInstance(QObject *parent = nullptr);
 
+public: //methods
+	void serialize(QXmlStreamWriter &xmlWriter);
+
+public: //getter
+
 	QUuid uuid() const;
-	void setUuid(const QUuid &uuid);
-
-	std::string sModuleTypeName() const;
-	void setSModuleTypeName(const std::string &sModuleTypeName);
-
-	std::string sModuleName() const;
-	void setSModuleName(const std::string &sModuleName);
-
-	std::string sDescription() const;
-	void setSDescription(const std::string &sDescription);
-
+	QString moduleTypeName() const;
+	QString moduleName() const;
+	QString description() const;
 	flaarlib::MODULE_TYPE moduleType() const;
-	void setModuleType(const flaarlib::MODULE_TYPE &moduleType);
-
 	flaarlib::DATA_TYPE dataType() const;
+	QPoint position();
+	QString moduleFunctionalName() const;
+
+public: //setter
+
+	void setUuid(const QUuid &uuid);
+	void setModuleTypeName(const QString &moduleTypeName);
+	void setModuleName(const QString &moduleName);
+	void setDescription(const QString &description);
+	void setModuleType(const flaarlib::MODULE_TYPE &moduleType);
 	void setDataType(const flaarlib::DATA_TYPE &dataType);
+	void setPosition(const QPoint &m_position);
+	void setPosition(const int x, const int y);
+	void setModuleFunctionalName(const QString &moduleFunctionalName);
 
 signals:
 
 public slots:
 
-private:
+private: //members
 	QUuid m_uuid;
-	std::string m_sModuleTypeName;
-	std::string m_sModuleName;
-	std::string m_sDescription;
+	QString m_sModuleTypeName;
+	QString m_sModuleName;
+	QString m_sModuleFunctionalName;
+	QString m_sDescription;
 	flaarlib::MODULE_TYPE m_moduleType;
 	flaarlib::DATA_TYPE m_dataType;
+	QPoint m_position;
 
 };
 
